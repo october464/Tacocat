d62a12d6e4ed81a84f6daab92c904e18fb8d4676
Jon <71294685+october464@users.noreply.github.com>
2020-10-06 16:56:03 -0400

    adding the nav bar back
    put my link to the portfolio nav bar

--- Scripts/MyCustom.js
+++ Scripts/MyCustom.js
@@ -1,37 +1,37 @@
-﻿//Palindrome
-function isPalindrome() {
-    let word = $("#myPalindrome").val();
-    let result = "";
-    //this loop is reversing the original word puts it in the result variable
-    for (let i = word.length - 1; i >= 0; i--) {
-        result += word.charAt(i);
-    }
-    // comparing the reversed word against the original word and if they are the same it's palindrome and if not this it's not a palindrome
-    if (result == word) {
-        $("#palResult").html(word + " " + "is a palindrome");
-    }
-    else {
-        $("#palResult").html(word + " " + "is not a palindrome");
-    }
-    
-
-}
-
-//function reverseWord() {
-//    let inputWord = document.getElementById("reverseString").value;
-//    let reverseWord = "";
-//    for (let loop = inputWord.length - 1; loop >= 0; loop--) {
-//        reverseWord += lowerInput.charAt(loop);
+﻿////Palindrome
+//function isPalindrome() {
+//    let word = $("#myPalindrome").val();
+//    let result = "";
+//    //this loop is reversing the original word puts it in the result variable
+//    for (let i = word.length - 1; i >= 0; i--) {
+//        result += word.charAt(i);
 //    }
-//    let otherReverse = lowerInput.split("").reverse().join("");
-//    if (lowerInput == reverseWord) {
-//        alert("otherReverse");
+//    // comparing the reversed word against the original word and if they are the same it's palindrome and if not this it's not a palindrome
+//    if (result == word) {
+//        $("#palResult").html(word + " " + "is a palindrome");
 //    }
 //    else {
-//        alert("try Again");
+//        $("#palResult").html(word + " " + "is not a palindrome");
 //    }
-//    document.getElementById()
+    
+
 //}
+
+function isPalindrome() {
+    let inpuWord = document.getElementById("myPalindrome").value;
+    let reverseWord = "";
+    for (let loop = inpuWord.length - 1; loop >= 0; loop--) {
+        reverseWord += inpuWord.charAt(loop);
+    }
+    let inpuWord = inpuWord.split("").reverse().join("");
+    if (inpuWord == reverseWord) {
+        alert("palResult");
+    }
+    else {
+        alert("Wrong");
+    }
+    document.getElementById("myPalindrome").innerHTML = palResult;
+}
 
 //document.getElementById("flip").addEventListener("click", function () {
 //    let inputWord = document.getElementById("reverseString").value
